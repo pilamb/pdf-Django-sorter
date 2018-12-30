a0d6e6fdcd3711678a27ec73f035d1b1b958db4d
pilamb <perikopalotes@gmail.com>
2018-12-30 20:19:37 +0100

    100% models coverage, adding pdfuploader app to coverage, PEP8 to models and pylintrc not needed

--- pdfuploader/models.py
+++ pdfuploader/models.py
@@ -22,107 +22,115 @@ class Archive(models.Model):
         verbose_name = "archive"
         verbose_name_plural = "archives"
 
-    file = models.FileField(upload_to='PDFSuploads/%Y/%m/%d/',
-                            validators=[validate_file_ext],
-                            verbose_name="File"
-                            )
+    file = models.FileField(
+        upload_to='PDFSuploads/%Y/%m/%d/',
+        validators=[validate_file_ext],
+        verbose_name="File"
+        )
 
-    title = models.CharField(max_length=200,
-                             verbose_name="Title"
-                             )
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Title"
+        )
 
-    upload_date = models.DateTimeField(auto_now_add=True,
-                                       verbose_name="Upload date-time"
-                                       )
+    upload_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Upload date-time"
+       )
 
-    creationdate = models.DateTimeField(null=True,
-                                        blank=True,
-                                        verbose_name="Creation date"
-                                        )
+    creationdate = models.DateTimeField(
+        null=True,
+        blank=True,
+        verbose_name="Creation date"
+        )
 
-    uploader = models.TextField(blank=True,
-                                max_length=25,
-                                null=True,
-                                verbose_name="Uploading user"
-                                )
+    uploader = models.TextField(
+        blank=True,
+        max_length=25,
+        null=True,
+        verbose_name="Uploading user"
+        )
 
     locked = models.BooleanField(
-                                default=False,
-                                verbose_name="Password protected"
-                                )
+        default=False,
+        verbose_name="Password protected"
+        )
 
-    tags = tagulous.models.TagField(  # Tag system
-                                get_absolute_url=lambda tag:
-                                reverse(
-                                    'pdfuploader.views.by_tag',
-                                    kwargs={'tag_slug': tag.slug}
-                                ),
-                                verbose_name="Tags",
-                                blank=True,
-                                force_lowercase=True,
-                                max_count=5,
-                                help_text="Enter comma separated words",
-                                )
-    hash_data = models.TextField(  # self generated: see views
-                                unique=True,
-                                max_length=250,
-                                verbose_name="Hash"
-                                )
+    tags = tagulous.models.TagField(
+        # Tag system
+        get_absolute_url=lambda tag:
+        reverse(
+            'pdfuploader.views.by_tag',
+            kwargs={'tag_slug': tag.slug}
+        ),
+        verbose_name="Tags",
+        blank=True,
+        force_lowercase=True,
+        max_count=5,
+        help_text="Enter comma separated words",
+        )
+    hash_data = models.TextField(
+        # self generated: see views
+        unique=True,
+        max_length=250,
+        verbose_name="Hash"
+        )
 
     author = models.TextField(
-                            default="",
-                            blank=True,
-                            help_text="PDF author.",
-                            verbose_name="Author"
-                            )
+        default="",
+        blank=True,
+        help_text="PDF author.",
+        verbose_name="Author"
+        )
 
     produced_by = models.TextField(
-                                default="",
-                                blank=True,
-                                help_text="Software/company who made it.",
-                                verbose_name="Done by "
-                                )
+        default="",
+        blank=True,
+        help_text="Software/company who made it.",
+        verbose_name="Done by "
+        )
 
     url = models.URLField(
-                        default="",
-                        blank=True,
-                        help_text="Website",
-                        verbose_name="URL"
-                        )
+        default="",
+        blank=True,
+        help_text="Website",
+        verbose_name="URL"
+        )
 
     pages = models.PositiveIntegerField(
-                                        default=0,
-                                        null=True,
-                                        blank=True,
-                                        verbose_name="Pages"
-                                        )
+        default=0,
+        null=True,
+        blank=True,
+        verbose_name="Pages"
+        )
 
-    size = models.PositiveIntegerField(  # bytes size
-                                        verbose_name="Size",
-                                        blank=True,
-                                        default=0,
-                                        null=True
-                                        )
+    size = models.PositiveIntegerField(
+        # bytes size
+        verbose_name="Size",
+        blank=True,
+        default=0,
+        null=True
+        )
     isbn = models.TextField(
-                            blank=True,
-                            default="",
-                            max_length=15,
-                            verbose_name="Commercial code"
-                           )
+        blank=True,
+        default="",
+        max_length=15,
+        verbose_name="Commercial code"
+       )
 
     producer = models.TextField(
-                                blank=True,
-                                default="",
-                                max_length=50,
-                                verbose_name="Software used"
-                                )
+        blank=True,
+        default="",
+        max_length=50,
+        verbose_name="Software used"
+        )
 
     creator = models.TextField(
-                                blank=True,
-                                default="",
-                                max_length=50,
-                                verbose_name="Other software used"
-                                )
+        blank=True,
+        default="",
+        max_length=50,
+        verbose_name="Other software used"
+        )
 
     def __unicode__(self):
         return unicode(self.title)
